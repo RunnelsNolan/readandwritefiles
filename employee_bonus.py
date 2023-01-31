2a56fd7ab85e7d929d8c12a373a7e2fe5d69d879
RunnelsNolan <nolanrunnels@icloud.com>
2023-01-30 23:21:46 -0600

    Fixed employee pay program

--- employee_bonus.py
+++ employee_bonus.py
@@ -6,9 +6,14 @@ reader = csv.reader(infile, delimiter = ',')
 
 field_names = next(reader)
 
-print(format(field_names[0], '8'), format('Full Name', '24'), format(field_names[3], '9'),field_names[4])
+print(format(field_names[0], '8'), format('Full Name', '24'), format(field_names[3], '9'),format(field_names[4],'10'), 'Total Pay')
 
 for row in reader:
-    print(format(row[0], '8'), format(row[1] +' ' + row[2], '25'), format(row[3], '10'), format(row[4], ''), sep = '')
+    salary = int(row[3])
+    bonus_rate = float(row[4])
+    bonus = salary * bonus_rate
+    total_pay = bonus + salary
+    print(format(row[0], '8'), format(row[1] +' ' + row[2], '25'), format(row[3], '7'), format(bonus,'10.2f'), format(total_pay,'12.2f'), sep = '')
+    input() #pause processing
 
 infile.close()
